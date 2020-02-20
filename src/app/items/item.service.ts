706b513a3152c8ff17481f3c25063960c5c97a6d
aaronshivers <aaron@moonmail.org>
2020-02-20 05:10:19 -0600

    add notes

--- src/app/items/item.service.ts
+++ src/app/items/item.service.ts
@@ -6,42 +6,54 @@ import { Item } from './item';
   providedIn: 'root',
 })
 export class ItemService {
-  // initialize an empty items array
+  // initialize the items array
   items: Item[] = [];
 
   constructor() {
+    // load items on initialization
     this.loadItems();
   }
 
-  // make items available publicly
+  // get items from items array
   getItems() {
     return this.items;
   }
 
-  // create item, save data, return id
   createItem(text: string): void {
+    // create item
     const item = { id: uuid(), text, completed: false };
 
+    // add item to items array
     this.items.push(item);
 
+    // save all items
     this.saveItems();
   }
 
   // delete item
   deleteItem(id: string): void {
+    // get the item index
     const itemIndex = this.getItemIndex(id);
 
     if (itemIndex >= 0) {
+      // remove the item from the items array
       this.items.splice(itemIndex, 1);
+
+      // save all items
       this.saveItems();
     }
   }
 
+  // change completed property
   toggleCompleted(id: string): void {
+    // get the item index
     const itemIndex = this.getItemIndex(id);
 
     if (itemIndex >= 0) {
+      // toggle the completed boolean
       this.items[itemIndex].completed = !this.items[itemIndex].completed;
+
+      // save all items
       this.saveItems();
     }
   }
